06854c61e7361971ac8cd9f6383f687731d24deb
Hui <coderslash@gmail.com>
2019-06-21 13:35:06 +0800

    format code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask ,send_from_directory ,render_template
+from flask import Flask, send_from_directory, render_template
 from urllib.parse import unquote_plus
 import os
+
 app = Flask(__name__)
 
 
@@ -9,19 +10,20 @@ def hello_world():
     return 'Hello World!'
 
 
-
 @app.route("/<video_name>/index.m3u8")
 def get_m3u8(video_name):
-    return send_from_directory(os.path.join("videos" ,video_name) ,"index.m3u8")
+    return send_from_directory(os.path.join("videos", video_name), "index.m3u8")
+
 
 @app.route("/<video_name>/index<int:idx>.ts")
-def  get_ts(video_name ,idx):
-    return send_from_directory(os.path.join("videos" ,video_name) ,"index"+str(idx)+".ts")
+def get_ts(video_name, idx):
+    return send_from_directory(os.path.join("videos", video_name), "index" + str(idx) + ".ts")
 
 
 @app.route("/video/<video_name>")
 def get_video_path(video_name):
-    return render_template("player.html" ,m3u8_url = "/" +video_name +"/index.m3u8",video_name =unquote_plus(video_name) )
+    return render_template("player.html", m3u8_url="/" + video_name + "/index.m3u8",
+                           video_name=unquote_plus(video_name))
 
 
 if __name__ == '__main__':
